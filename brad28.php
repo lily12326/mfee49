5220040d9f05acf161f4aaa3c132e58e87b70f22
lily12326 <lilychu23@gmail.com>
2024-01-17 10:17:59 +0800

    w, r, r+, a

--- brad28.php
+++ brad28.php
@@ -1,10 +1,13 @@
 <?php
-    $fp = @fopen('dir1/file3.txt', 'r') or die('server busy');  //原本沒有file3
+    $fp = @fopen('dir1/file3.txt', 'a') or die('server busy');  //原本沒有file3
+
+    fwrite($fp, '123');
 
     fclose($fp);
     echo 'objk';
 ?>
 
 //讀有多種讀法，寫法較單一
-
+//@ => 如果打不開/沒有這個file，會報錯warning，加了＠就不會報warining（只出現'server busy'）
+//如果沒有辦法讀寫執行 -> 開啟dir1的權限（開啟母資料夾權限，才能讀寫執行子檔案的內容）
 
